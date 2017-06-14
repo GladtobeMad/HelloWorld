9af2b9e5132992946ca46896d33e5f21a89355ed
GladtobeMad <pl222461@ahschool.com>
2017-06-14 08:37:05 -0400

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Vlad on 6/14/17.
+ */
+public class HelloWorld {
+
+    public void say() {
+        System.out.println("Hello, World!");
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Vlad on 6/14/17.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        HelloWorld hello = new HelloWorld();
+        hello.say();
+    }
+
+}
